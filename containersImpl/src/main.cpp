1b095013922e4b37ed09d0bd83ea97c40dd9ded0
xin lu <xin.lu@amadeus.com>
2019-11-27 18:26:12 +0100

    create makefile && reorganized directories

---
+++ containersImpl/src/main.cpp
@@ -0,0 +1,14 @@
+#include "include/llstack.hpp"
+#include <iostream>
+
+int main(int argc, char* argv[]){
+	using namespace Containers;
+	llstack<int>* stack = new llstack<int>();
+	for(int i = 0 ; i < 10 ; ++i){
+		stack->push(i);
+	}
+	while(!stack->isEmpty()){
+		std::cout << stack->pop() << std::endl;
+	}
+	return 0;
+}
